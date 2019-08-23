d579f1ad6a1b56e1f00f42a2165d8cd5aef0ca26
Luis Dalmolin <luis.nh@gmail.com>
2019-08-23 07:48:14 -0300

    Added the ability to disable broadcasting

--- src/LockEvent.php
+++ src/LockEvent.php
@@ -49,4 +49,14 @@ abstract class LockEvent implements ShouldBroadcast
     {
         return config('model_locking.broadcast_as.'.static::class) ?: static::class;
     }
+
+    /**
+     * Determine if this event should broadcast.
+     *
+     * @return bool
+     */
+    public function broadcastWhen()
+    {
+        return config('model_locking.broadcast_enabled', true);
+    }
 }
